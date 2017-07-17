0f16f84c4c0da23b7e6913aa42115cb761d4ecdc
Hung Yu-Ting <r96941046@gmail.com>
2017-07-17 15:10:17 +0800

    Prediction on pi

--- pi.py
+++ pi.py
@@ -1,15 +1,22 @@
 import os
 import sys
-import shlex
 import shutil
+import tensorflow as tf
 import pygame
+import numpy as np
 
+from scipy import ndimage
+from scipy.misc import imresize
 from subprocess import Popen
 
 FILENAME = 'webcam.jpg'
 CAPTUREDFILENAME = 'captured.jpg'
 COMMAND = ['fswebcam', '--device', '/dev/video0', FILENAME, '--loop', '0.1']
 CWD = os.getcwd()
+
+PIXEL_DEPTH = 255.0
+IMAGE_SIZE = 32
+MAX_DIGITS = 5
 
 pygame.init()
 
@@ -28,7 +35,7 @@ while True:
         image = pygame.transform.scale(image, (640, 480))
     except:
         print('Image IO error')
-        
+
     screen.blit(image, (0, 0))
 
     pygame.display.update()
@@ -45,6 +52,45 @@ while True:
         captured = pygame.transform.scale(captured, (64, 64))
 
         screen.blit(captured, (200, 480))
+
+        image_file = ndimage.imread(dst_file).astype(np.float32)
+        grey = np.dot(image_file, [0.299, 0.587, 0.114])
+        resized = imresize(grey, (IMAGE_SIZE, IMAGE_SIZE))
+        normalized = (resized - PIXEL_DEPTH / 2) / PIXEL_DEPTH
+        normalized = normalized.reshape((IMAGE_SIZE, IMAGE_SIZE, 1))
+
+        tf_data = np.zeros((1, IMAGE_SIZE, IMAGE_SIZE, 1), dtype=np.float32)
+        tf_data[0] = normalized
+
+        graph = tf.get_default_graph()
+
+        with tf.Session(graph=graph) as session:
+
+            loader = tf.train.import_meta_graph('svhn.meta')
+            loader.restore(session, tf.train.latest_checkpoint('./'))
+
+            tf_data = graph.get_tensor_by_name('data:0')
+            keep_prob = graph.get_tensor_by_name('keep_prob:0')
+            prediction = graph.get_tensor_by_name('prediction:0')
+
+            feed_dict = {
+                tf_data: tf_data,
+                keep_prob: 1.0
+            }
+
+            p = prediction.eval(feed_dict)
+            print(p)
+
+            digitized = np.split(np.argmax(p, axis=1), MAX_DIGITS)
+            digits = []
+
+            for digit in digitized:
+                if digit == 10:
+                    digit = ''
+                digits.append(str(digit))
+
+            result = ''.join(digits)
+            print('Result: %s' % result)
 
     if quit_rect.collidepoint(pos) and pressed_l:
         pygame.quit()
